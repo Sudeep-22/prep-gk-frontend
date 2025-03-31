e05393a89bdc52856a20ad0a6b01203f96dfcf8c
SudeepPoojari <114012832+Sudeep-22@users.noreply.github.com>
2025-03-31 07:50:39 +0530

    Changed delete and update buttons settings

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -12,8 +12,8 @@ const NoteItem = (props) => {
     <h5 className="card-title">{note.title}</h5>
     <h6 className="card-subtitle mb-2 text-body-secondary">{note.tag}</h6>
     <p className="card-text">{note.content}</p>
-    <i className="fa-solid fa-trash mx-2" onClick={()=>{deleteNote(note._id);props.setAlert("danger","Note deleted successfully");}}/>
-    <i className="fa-regular fa-pen-to-square" onClick= {() => {updateNote(note)}}/>
+    <i className="fa-solid fa-trash mx-2 cursor-pointer" onClick={()=>{deleteNote(note._id);props.setAlert("danger","Note deleted successfully");}}/>
+    <i className="fa-regular fa-pen-to-square cursor-pointer" onClick= {() => {updateNote(note)}}/>
   </div>
   </div>
   </div>
